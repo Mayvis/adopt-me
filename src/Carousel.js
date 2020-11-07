b4e1f236fe835965b864bb569845cf227d5acceb
Mayvis <kevin73911@gmail.com>
2020-11-07 10:17:18 +0800

    Creating an image carousel

---
+++ src/Carousel.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+    return { photos };
+  }
+
+  handleIndexClick() {}
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              src={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              alt="animal"
+              key={index}
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
